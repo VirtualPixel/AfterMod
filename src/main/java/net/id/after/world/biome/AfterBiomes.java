1f95c64c569709aad9dc494519cd925e8572f7df
Justin Wells <wellsmjustin@gmail.com>
2022-04-10 13:28:44 -0400

    * Created new biome features * Customized options for biome

--- src/main/java/net/id/after/world/biome/AfterBiomes.java
+++ src/main/java/net/id/after/world/biome/AfterBiomes.java
@@ -7,17 +7,55 @@ import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeEffects;
+import net.minecraft.world.biome.GenerationSettings;
+import net.minecraft.world.biome.SpawnSettings;
+import net.minecraft.world.gen.feature.DefaultBiomeFeatures;
 
 import static net.id.after.After.MOD_ID;
 
 public class AfterBiomes {
     public static final RegistryKey<Biome> AFTER_BIOME_KEY = RegistryKey.of(Registry.BIOME_KEY, new Identifier(MOD_ID, "after_biome"));
-    private static Biome AFTER;
+    private static Biome AFTER(){
+        SpawnSettings.Builder spawnSettings = new SpawnSettings.Builder();
+        //DefaultBiomeFeatures.addFarmAnimals(spawnSettings);
+
+        GenerationSettings.Builder generationSettings = new GenerationSettings.Builder();
+        DefaultBiomeFeatures.addLandCarvers(generationSettings);
+        DefaultBiomeFeatures.addTallBirchTrees(generationSettings);
+        DefaultBiomeFeatures.addBirchTrees(generationSettings);
+        DefaultBiomeFeatures.addDungeons(generationSettings);
+        DefaultBiomeFeatures.addMineables(generationSettings);
+        //DefaultBiomeFeatures.addDefaultOres(generationSettings);
+        DefaultBiomeFeatures.addDefaultDisks(generationSettings);
+        DefaultBiomeFeatures.addSprings(generationSettings);
+        DefaultBiomeFeatures.addFrozenTopLayer(generationSettings);
+
+        return (new Biome.Builder())
+                .precipitation(Biome.Precipitation.RAIN)
+                .category(Biome.Category.NONE)
+                .precipitation(Biome.Precipitation.NONE)
+                //.depth(0.125F)
+                //.scale(0.05F)
+                .temperature(0.8F)
+                .downfall(0.4F)
+                .category(Biome.Category.FOREST)
+                .effects((new BiomeEffects.Builder())
+                        .waterColor(16743034)
+                        .waterFogColor(4721171)
+                        .fogColor(7895160)
+                        .skyColor(197379)
+                        .grassColor(4150633)
+                        .build())
+                .spawnSettings(spawnSettings.build())
+                .generationSettings(generationSettings.build())
+                .build();
+    }
 
     public static void init(){
         Registry.register(Registry.CHUNK_GENERATOR, new Identifier("the_after", "the_after"), AfterChunkGenerator.CODEC);
 
-        Registry.register(BuiltinRegistries.BIOME, AFTER_BIOME_KEY.getValue(), AFTER);
+        Registry.register(BuiltinRegistries.BIOME, AFTER_BIOME_KEY.getValue(), AFTER());
     }
 
 }
