f076d2b684ee15072cdee009515f4908ffd64815
Justin Wells <wellsmjustin@gmail.com>
2024-08-09 15:41:02 -0400

    Initial commit for 1.21 branch

---
+++ src/client/java/com/justinwells/theafter/TheAfterProjectClient.java
@@ -0,0 +1,10 @@
+package com.justinwells.theafter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheAfterProjectClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
